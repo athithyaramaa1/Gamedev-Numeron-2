539438151673900e32fd9ea88fa6196679f7e880
athithyaramaa1 <athithya.ramma@kalvium.community>
2024-02-26 10:35:47 +0530

    Numeron2 functional Game

--- game.js
+++ game.js
@@ -1,11 +1,118 @@
-// Iteration 2: Generating two random numbers (0 to 100) and displaying the same in the game.html
+let num1Div = document.getElementById('number1');
+let num2Div = document.getElementById('number2');
+let num3Div = document.getElementById('number3');
 
-// Iteration 3: Creating variables required to make the game functional
+const plusButton = document.querySelector('#plus');
+const minusButton = document.querySelector('#minus');
+const multiplyButton = document.querySelector('#mul');
+const divideButton = document.querySelector('#divide');
+const moduloButton = document.querySelector('#modulus');
 
-// Iteration 4: Creating a variable for number 3 and a variable for storing the html element with the Id "number3"
+let timer = document.getElementById('timer');
 
-// Iteration 5: Creating a randomise function to make the game functional
+let num1;
+let num2;
+let num3;
+let clock = 20;
+timer.innerHTML = clock;
+let score = 0;
+let interval;
 
-// Iteration 6: Making the Operators (button) functional
+function randomGenerator() {
+  num1 = Math.floor(Math.random() * 100) + 1;
+  num2 = Math.floor(Math.random() * 100) + 1;
 
-// Iteration 7: Making Timer functional
+  let randomOperator = Math.floor(Math.random() * 5) + 1;
+
+  if (randomOperator == 1) {
+    num3 = num1 + num2;
+  } else if (randomOperator == 2) {
+    num3 = num1 - num2;
+  } else if (randomOperator == 3) {
+    num3 = num1 * num2;
+  } else if (randomOperator === 4) {
+    num3 = Math.floor(num1 / num2);
+  } else if (randomOperator == 5) {
+    num3 = num1 % num2;
+  }
+
+  num1Div.innerHTML = num1;
+  num2Div.innerHTML = num2;
+  num3Div.innerHTML = num3;
+}
+
+randomGenerator();
+
+plusButton.addEventListener('click', function () {
+  if (num1 + num2 == num3) {
+    score++;
+    randomGenerator();
+    clearInterval(interval);
+    resetTimer();
+    localStorage.setItem('score', score);
+  } else {
+    window.location.href = './gameover.html';
+  }
+});
+
+minusButton.addEventListener('click', function () {
+  if (num1 - num2 == num3) {
+    score++;
+    randomGenerator();
+    clearInterval(interval);
+    resetTimer();
+    localStorage.setItem('score', score);
+  } else {
+    window.location.href = './gameover.html';
+  }
+});
+
+multiplyButton.addEventListener('click', function () {
+  if (num1 * num2 == num3) {
+    score++;
+    randomGenerator();
+    clearInterval(interval);
+    resetTimer();
+    localStorage.setItem('score', score);
+  } else {
+    window.location.href = './gameover.html';
+  }
+});
+
+divideButton.addEventListener('click', function () {
+  if (num1 / num2 == num3) {
+    score++;
+    randomGenerator();
+    clearInterval(interval);
+    resetTimer();
+    localStorage.setItem('score', score);
+  } else {
+    window.location.href = './gameover.html';
+  }
+});
+
+moduloButton.addEventListener('click', function () {
+  if (num1 % num2 == num3) {
+    score++;
+    randomGenerator();
+    clearInterval(interval);
+    resetTimer();
+    localStorage.setItem('score', score);
+  } else {
+    window.location.href = './gameover.html';
+  }
+});
+
+function resetTimer() {
+  clock = 20;
+  timer.innerHTML = clock;
+  interval = setInterval(function () {
+    clock = clock - 1;
+    timer.innerHTML = clock;
+    if (clock == 0) {
+      window.location.href = './gameover.html';
+    }
+  }, 1000);
+}
+
+resetTimer();
--- score.js
+++ score.js
@@ -1,1 +1,9 @@
 // Iteration 8: Making scoreboard functional
+const scoreBoard = document.getElementById('score-board');
+const playAgainButton = document.getElementById('play-again-button');
+
+let totalScore = localStorage.getItem('score');
+scoreBoard.innerHTML = totalScore;
+playAgainButton.addEventListener('click', function () {
+  window.location.href = 'game.html';
+});
